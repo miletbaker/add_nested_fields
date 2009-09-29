42c71dcb0368cf2f1583ecf2f89f2aac8e98f11a
Jon Baker <jon@gotripod.com>
2009-09-29 17:47:40 +0100

    Updated code to correct label and add confirmation on delete

--- lib/add_nested_fields.rb
+++ lib/add_nested_fields.rb
@@ -4,11 +4,11 @@ module AddNestedFields
 		def add_nested_fields_for(form_builder, field, dom_id, *args)
 			options = {
 				:partial => field.to_s.singularize,
-				:label => field.to_s.singularize.titleize,
+				:label => "Add #{field.to_s.singularize.titleize}",
 				:object => field.to_s.classify.constantize.new
 			}.merge(args.extract_options!)
 
-			link_to_function("&nbsp;Add #{options[:label]}") do |page|
+			link_to_function("#{options[:label]}") do |page|
 				form_builder.fields_for field, options[:object] , :child_index => 'NEW_RECORD' do |f|
 			  	html = render(:partial => options[:partial], :locals => { :f => f })
 					page << "$('#{dom_id}').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
@@ -16,14 +16,18 @@ module AddNestedFields
 			end
 		end
 
-		def remove_nested_fields_for(form_builder, class_id)
+		def remove_nested_fields_for(form_builder, class_id, *args)
+			options = {
+				:label => 'remove'
+			}.merge(args.extract_options!)
+			confirm = "if (confirm('Are you sure you would like to delete this #{form_builder.object.class.to_s.underscore.humanize.downcase}?'))"
 			if form_builder.object.new_record?
-				link_to_function("(remove)") do |page|
-					page << "$(this).up('.#{class_id}').remove()"
+				link_to_function(options[:label]) do |page|
+					page << "#{confirm} $(this).up('.#{class_id}').remove()"
 				end
 			else
-				form_builder.hidden_field( :_delete, :value => "0") + link_to_function("(remove)") do |page|
-					page << "$(this).up('.#{class_id}').hide();$(this).previous().value = 1"
+				form_builder.hidden_field( :_delete, :value => "0") + link_to_function(options[:label]) do |page|
+					page << "#{confirm} $(this).up('.#{class_id}').hide();$(this).previous().value = 1"
 				end
 			end
 		end
